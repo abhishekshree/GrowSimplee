afab99570a8f80fda0f1f82302d222901091c6cc
Abhishek Shree <abhi1234456@gmail.com>
2023-01-11 20:02:07 +0530

    `main.py` to test modules

---
+++ main.py
@@ -0,0 +1,12 @@
+from config.config import Variables
+import pandas as pd
+from location.geocoding import Geocoding
+
+
+# example for location module
+data_df = pd.read_excel("data/bangalore_dispatch_address_finals.xlsx")
+g = Geocoding(Variables.bingAPIKey, data_df)
+
+res = g.generate()
+
+print(res)
